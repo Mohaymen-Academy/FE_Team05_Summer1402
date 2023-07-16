eb863fb0ff2726f5994e96b5b0fa1612cbe48b5e
abolfazl omidvar <abolfazl.omidvar46@gmail.com>
2023-07-16 21:58:35 +0330

    fix: index.ts files names

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@
 
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -18,10 +18,7 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
     '@typescript-eslint/no-non-null-assertion': 'off',
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import {useState} from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import {Container} from './Components/Container';
 import {Header} from './Components';
-import {Carousel} from './Components';
-import './App.css';
+import {Carousel} from './Components/Carousel';
 
 function App() {
   return (
@@ -12,7 +8,6 @@ function App() {
       <Header />
       <Carousel />
     </Container>
-    </>
   );
 }
 
