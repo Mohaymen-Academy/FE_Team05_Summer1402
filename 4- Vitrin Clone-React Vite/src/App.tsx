7f30bbfa2c87d526eccfcf80aaeddbce8b40aa16
abolfazl omidvar <abolfazl.omidvar46@gmail.com>
2023-07-26 17:23:43 +0330

    work on carousel

--- 4- Vitrin Clone-React Vite/src/App.tsx
+++ 4- Vitrin Clone-React Vite/src/App.tsx
@@ -1,13 +1,7 @@
 import Carousel from './Components/Carousel/Carousel';
 
-import {carouselImages} from './util/Constants';
-
 function App() {
-  return (
-    <>
-      <div className="bg-rose-100">سلام</div>
-    </>
-  );
+  return <Carousel />;
 }
 
 export default App;
--- 4- Vitrin Clone-React Vite/src/Components/Carousel/Carousel.tsx
+++ 4- Vitrin Clone-React Vite/src/Components/Carousel/Carousel.tsx
@@ -1,24 +1,121 @@
-import React from 'react';
+import Carousel3d from 'react-spring-3d-carousel';
+import {useState, useEffect, useRef} from 'react';
+import {config} from 'react-spring';
+import {carouselImages} from '../../util/Constants';
+import {v4 as uuidv4} from 'uuid';
+import Card from './Card';
 
-type CarouselProps = {
-  imgArray: string[];
-};
+export default function Carousel() {
+  const [offsetRadius, setOffsetRadius] = useState(2);
+  const [goToSlide, setGoToSlide] = useState<number>(2);
 
-const Carousel: React.FC<CarouselProps> = ({imgArray}) => {
+  const startOffset = useRef(0);
+  const endOffset = useRef(0);
+  const slide = useRef(goToSlide);
+
+  let cards: {key: string; content: React.ReactNode}[] = carouselImages.map((img) => ({
+    key: uuidv4(),
+    content: <Card imageSrc={img} />,
+  }));
+
+  const slides = cards.map((element, index) => {
+    return {...element, onClick: () => setGoToSlide(index)};
+  });
+
+  const determineDirection = (delta: number) => {
+    if (delta === 0) return;
+
+    if (delta > 0) {
+      if (goToSlide === slides.length) {
+        setGoToSlide(0);
+      } else {
+        setGoToSlide(goToSlide + 1);
+      }
+    }
+    if (delta < 0) {
+      if (goToSlide === 0) {
+        setGoToSlide(slides.length - 1);
+      } else {
+        setGoToSlide(goToSlide - 1);
+      }
+    }
+
+    startOffset.current = 0;
+    endOffset.current = 0;
+  };
+
+  useEffect(() => {
+    const slideAutomate = setInterval(() => {
+      const slideNum = slide.current;
+
+      if (slideNum === slides.length - 1) {
+        setGoToSlide(0);
+        slide.current = 0;
+      } else {
+        setGoToSlide(slide.current + 1);
+      }
+      // setGoToSlide(slide.current - 1);
+      slide.current++;
+    }, 2000);
+
+    //cleanUp function
+    return () => clearInterval(slideAutomate);
+  }, []);
+
+  useEffect(() => {
+    console.log(goToSlide);
+  }, [goToSlide]);
+
+  const dragStart = (e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) => {
+    //@ts-ignore
+    startOffset.current = e.nativeEvent.offsetX;
+  };
+
+  const dragEnd = (e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) => {
+    //@ts-ignore
+    endOffset.current = e.nativeEvent.offsetX;
+
+    determineDirection(startOffset.current - endOffset.current);
+
+    startOffset.current = 0;
+    endOffset.current = 0;
+  };
+
+  const touchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    startOffset.current = e.touches[0].clientX;
+  };
+
+  const touchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+    endOffset.current = e.changedTouches[0].clientX;
+    determineDirection(startOffset.current - endOffset.current);
+  };
+
   return (
-    <section className="max-w-6xl m-auto mb-12">
-      <div className="w-[90%] m-auto overflow-hidden hidden">
-        <div className="gallery-container flex items-center h-[330px] sm:h-[478px] mx-auto max-w-[1000px] relative">
-          {imgArray.map((img) => (
-            <a className="gallery-item" data-index="1" href="#">
-              <img className="carousel-image" src={img} alt="carousel image" />
-            </a>
+    <div className="w-[40%] m-auto">
+      <div
+        onMouseDown={dragStart}
+        onMouseUp={dragEnd}
+        onTouchStart={touchStart}
+        onTouchEnd={touchEnd}
+        className="h-[420px] w-full relative"
+      >
+        <Carousel3d
+          slides={slides}
+          goToSlide={goToSlide}
+          offsetRadius={offsetRadius}
+          showNavigation={false}
+          animationConfig={config.gentle}
+        />
+        <div className="flex flex-row-reverse gap-2 absolute z-50 bottom-5 left-1/2 -translate-x-1/2 bg-slate-500/70 p-2 rounded-xl opacity-70 hover:opacity-100 transition">
+          {slides.map((sld, i) => (
+            <div
+              key={sld.key}
+              className={`bullet ${goToSlide === i ? 'bullet-active' : ''}`}
+              onClick={sld.onClick}
+            ></div>
           ))}
-          <div className="flex flex-row-reverse gap-2 absolute z-50 bottom-2 md:bottom-0 left-1/2 -translate-x-1/2 bg-slate-500/30 p-2 rounded-xl opacity-70 hover:opacity-100 transition"></div>
         </div>
       </div>
-    </section>
+    </div>
   );
-};
-
-export default Carousel;
+}
