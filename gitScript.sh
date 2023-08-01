6d9d634e23fce717a77ff9947f723ce0b8c93af5
abolfazl-omidvarr <abolfazl.omidvar46@gmail.com>
2023-08-02 01:17:24 +0430

    this is a test

---
+++ gitScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if both arguments are provided
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 [bName] [cMessage]"
+  exit 1
+fi
+
+cMessage="$2"
+
+# Perform git commands
+git add -A
+git commit -m "$cMessage"
+
+if [ -n "$1" ]; then
+  bName="$1"
+  git checkout -b "$bName"
+  git push origin "$bName"
+fi
